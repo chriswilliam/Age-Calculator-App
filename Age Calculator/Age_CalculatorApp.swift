fa9d809f8d6480cc6237926c6f71e64873e2a85a
Christopher William <chriswilliam2000@icloud.com>
2023-02-18 23:30:35 +1100

    Initial Commit

---
+++ Age Calculator/Age_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Age_CalculatorApp.swift
+//  Age Calculator
+//
+//  Created by Chris William on 18/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Age_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Age Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Age Calculator
+//
+//  Created by Chris William on 18/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
